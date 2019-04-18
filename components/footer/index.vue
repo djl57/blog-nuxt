d248c3ab61c7ba79db50327188e56d43691c4ca5
djl57 <18367345931@163.com>
2019-04-18 15:25:34 +0800

    co

---
+++ components/footer/index.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="footer">
+    <div class="wrap">
+      <div class="left">
+        <p class="ft-title">推 荐</p>
+        <ul>
+          <li v-for="(item, index) in recom" :key="index" class="item">
+            <a :href="item.link">{{ item.name }}</a>
+          </li>
+        </ul>
+      </div>
+      <div class="middle">
+        <p class="ft-title">资 源</p>
+        <ul>
+          <li v-for="(item, index) in sourse" :key="index" class="item">
+            <a :href="item.link">{{ item.name }}</a>
+          </li>
+        </ul>
+      </div>
+      <div class="right">
+        <p class="ft-title">联 系 我</p>
+        <div class="get" :class="showEmail?'email':'no-email'" @click="getEmail">{{ email }}</div>
+      </div>
+    </div>
+    <div class="copyright">
+      Copyright © 2019 - <span class="author">DJLUN.</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      recom: [
+        { link: 'https://unsplash.com/', name: 'unsplash' },
+        { link: 'https://marketplace.ghost.org/', name: 'ghost' },
+      ],
+      sourse: [
+        { link: 'https://github.com/djl57', name: 'github' },
+      ],
+      showEmail: false,
+      email: '获 取'
+    }
+  },
+  methods: {
+    getEmail() {
+      this.showEmail = !this.showEmail
+      this.showEmail ? this.email = 'djl5700@163.com' : this.email = '获 取'
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  border-top: 1px solid #eee;
+  .wrap {
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    padding: 0 40px;
+    .left,
+    .middle,
+    .right {
+      flex: 1;
+      .ft-title {
+        color: #aaa;
+        font-size: 14px;
+        letter-spacing: 2px;
+        padding-bottom: 20px;
+      }
+      .item {
+        height: 32px;
+        line-height: 32px;
+        color: #330;
+      }
+    }
+    .left {
+      padding: 60px 60px 60px 0;
+    }
+    .middle {
+      border-left: 1px solid #eee;
+      border-right: 1px solid #eee;
+      padding: 60px;
+    }
+    .right {
+      padding: 60px 0 60px 60px;
+      .get {
+        width: 150px;
+        height: 30px;
+        line-height: 30px;
+        font-size: 12px;
+        text-align: center;
+        cursor: pointer;
+      }
+      .no-email {
+        color: #fff;
+        background-color: #330; 
+        transition: background-color 0.75s;
+      }
+      .email {
+        color: #330;
+        background-color: #fff; 
+        border: 1px solid #330;
+        transition: background-color 0.75s;
+      }
+    }
+  }
+  .copyright {
+    border-top: 1px solid #eee;
+    color: #aaa;
+    font-size: 11px;
+    padding: 30px 0;
+    text-align: center;
+    text-transform: uppercase;
+    .author {
+      color: #330;
+    }
+  }
+}
+</style>
---
+++ components/header/index.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="header">
+    <div class="wrap">
+      <div class="brand">
+        <h2 class="inline-block">DJLUN.</h2>
+        <p class="desc inline-block">Thoughts, stories and ideas.</p>
+        <!-- <p class="desc inline-block">Codes.</p> -->
+      </div>
+      <nav class="nav">
+        <ul class="menu inline-block">
+          <li class="inline-block item" :class="itemClass(item.active)" v-for="(item, index) in navs" :key="index"  
+            @mouseover="mouseover1(index)" @mouseleave="mouseleave1(index)">
+            <nuxt-link :to="'/'+item.link">{{ item.name }}</nuxt-link>
+          </li>
+        </ul>
+        <div class="search inline-block cursor-pointer" :class="{'search-over': overSearch}" 
+          @mouseover="mouseover" @mouseleave="mouseleave">
+          <i class="el-icon-search"></i>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navs: [
+        { name: 'home', link: '', active: false },
+        { name: '分类', link: 'category', active: false },
+        { name: '登录', link: 'signin', active: false },
+        { name: '注册', link: 'signup', active: false }
+      ],
+      overSearch: false,
+      overli: false
+    }
+  },
+  methods: {
+    mouseover() {
+      this.overSearch = true
+    },
+    mouseleave() {
+      this.overSearch = false
+    },
+    mouseover1(index) {
+      for(let prop of this.navs) {
+        prop.active = false
+      }
+      this.navs[index].active = true
+      this.overli = true
+    },
+    mouseleave1(index) {
+      for(let prop of this.navs) {
+        prop.active = false
+      }
+      this.overli = false
+    },
+    itemClass(active) {
+      return {
+        'item-over': this.overSearch,
+        'item-active': active,
+        'item-no-active': this.overli && !active
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  height: 65px;
+  padding: 50px 0 40px;
+  letter-spacing: 1px;
+  .wrap {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 0 40px;
+    height: 65px;
+    display: flex;
+    justify-content: space-between;
+    .brand {
+      flex: 0 0 380px;
+      line-height: 65px;
+      .desc {
+        color: #aaa;
+        margin-left: 20px;
+        font-size: 15px;
+      }
+    }
+    .nav {
+      flex: 1;
+      text-align: right;
+      text-transform: uppercase;
+      .menu {
+        font-size: 12px;
+        line-height: 73px;
+        .item {
+          margin-right: 30px;
+          transition: all 0.75s;
+          &::after {
+            content: '';
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: 10px;
+            height: 1px;
+            transform: scaleY(0.5);
+            background-color: #aaa;
+            opacity: 0;
+            transition: all 0.75s;
+          }
+        }
+        .item-over {
+          color: #aaa;
+        }
+        .item-active {
+          position: relative;
+          &::after {
+            content: '';
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: 25px;
+            height: 1px;
+            transform: scaleY(0.5);
+            background-color: #aaa;
+            opacity: 1;
+            transition: all 0.75s;
+          }
+        }
+        .item-no-active {
+          transition: all 0.75s;
+          color: #aaa;
+        }
+      }
+      .search {
+        font-size: 16px;
+        padding: 10px;
+        position: relative;
+        top: 1px;
+      }
+      .search-over {
+        border-radius: 50%;
+        background-color: #eee;
+      }
+    }
+  }
+}
+</style>
